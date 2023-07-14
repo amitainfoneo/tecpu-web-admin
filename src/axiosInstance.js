f69ec1dc45e94c58e53cb41c3c55a526cf0c41ae
amita <amitinfoneo@gmail.com>
2023-07-14 10:45:40 +0530

    remove console

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from './config.json';
 let token = localStorage.getItem('token');
-console.log("token==>",token)
+
 
 const axiosInstance = axios.create({
   baseURL: config.serverUrl,
